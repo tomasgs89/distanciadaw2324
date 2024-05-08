28d36178fda4e1dbb458c45aba3f8b9c72a94467
Tomas Gandolfo <tgandolfo89@hotmail.com>
2024-05-08 14:42:31 +0200

    Segundo commit

---
+++ practica-gandolfo.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+* Dos funciones documentadas - Tarea DAw 5.
+*
+*@author TomÃ¡s Gandolfo SÃ¡nchez
+*@version 1.0
+*
+*/
+
+/**
+*Funcion para la suma de dos nÃºmeros.
+*
+*@param integer $a Primer nÃºmero.
+*@param integer $b Segundo nÃºmero.
+*@return integer $resultado Devuelve la suma de los dos nÃºmeros.
+*@internal Anotacion para desarrolladores.
+*/
+
+function sumar ($a, $b){
+    $resultado = $a + $b;
+    return $resultado;
+}
+
+/**
+*Funcion para restar dos nÃºmeros.
+*
+*@param integer $a Primer nÃºmero.
+*@param integer $b Segundo nÃºmero.
+*@return integer $resultado Devuelve la resta  de los dos nÃºmeros.
+*@internal Anotacion para desarrolladores.
+*/
+
+function restar ($a, $b){
+     $resultado = $a - $b;
+     return $resultado;
+}
+
+
+
+?>
